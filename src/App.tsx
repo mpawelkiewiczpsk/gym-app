65c14261865f9dd658ef036ce7903fb699134860
mpawelkiewiczpsk <mpawelkiewicz@tu.kielce.pl>
2024-03-15 08:49:37 +0100

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Layout, Menu, theme, Result, Button } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Footer } = Layout;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const items = [
+    {
+        key: 'home',
+        label: 'Home'
+    },
+    {
+        key: 'prices',
+        label: 'Prices'
+    },
+    {
+        key: 'login',
+        label: 'Login'
+    }
+]
 
-export default App
+const App: React.FC = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', alignItems: 'center' }}>
+                <div className="demo-logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={items}
+                    style={{ flex: 1, minWidth: 0 }}
+                />
+            </Header>
+            <Content style={{ padding: '0 48px' }}>
+                <div
+                    style={{
+                        background: colorBgContainer,
+                        minHeight: 280,
+                        padding: 24,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Result
+                        status="warning"
+                        title="Zastanów się zanim kupisz"
+                        extra={
+                            <Button type="primary" key="console">
+                                Kup karnet
+                            </Button>
+                        }
+                    />
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Gym APP
+            </Footer>
+        </Layout>
+    );
+};
+
+export default App;
